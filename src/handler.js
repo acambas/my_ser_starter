0f143bb85f939ff3b4542dd6433ef89bea4e0cfc
acambas <aleksandarcambas@gmail.com>
2017-10-10 00:22:20 +0100

    try gzip

--- src/handler.js
+++ src/handler.js
@@ -1,6 +1,26 @@
 import awsServerlessExpress from 'aws-serverless-express';
 import app from './server';
 
-const server = awsServerlessExpress.createServer(app);
+const binaryMimeTypes = [
+  'application/javascript',
+  // 'application/json',
+  'application/octet-stream',
+  'application/xml',
+  'font/eot',
+  'font/opentype',
+  'font/otf',
+  'image/jpeg',
+  'image/png',
+  'image/svg+xml',
+  'text/comma-separated-values',
+  'text/css',
+  'text/html',
+  'text/javascript',
+  'text/plain',
+  'text/text',
+  'text/xml',
+];
+
+const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes);
 export const express = (event, context) =>
   awsServerlessExpress.proxy(server, event, context);
