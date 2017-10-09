335b2a534ba178ae9af17c2905997475f377885c
acambas <aleksandarcambas@gmail.com>
2017-10-10 00:32:50 +0100

    gzip fail, fix base url

--- src/handler.js
+++ src/handler.js
@@ -1,26 +1,6 @@
 import awsServerlessExpress from 'aws-serverless-express';
 import app from './server';
 
-const binaryMimeTypes = [
-  'application/javascript',
-  // 'application/json',
-  'application/octet-stream',
-  'application/xml',
-  'font/eot',
-  'font/opentype',
-  'font/otf',
-  'image/jpeg',
-  'image/png',
-  'image/svg+xml',
-  'text/comma-separated-values',
-  'text/css',
-  'text/html',
-  'text/javascript',
-  'text/plain',
-  'text/text',
-  'text/xml',
-];
-
-const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes);
+const server = awsServerlessExpress.createServer(app);
 export const express = (event, context) =>
   awsServerlessExpress.proxy(server, event, context);
