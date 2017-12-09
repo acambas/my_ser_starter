ebb409c0119af9c6d25c8dea48032b3f38eb7bdb
acambas <aleksandarcambas@gmail.com>
2017-12-09 13:07:53 +0000

    dep update and remove semantic ui

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Pages from './pages';
 import './style/global-style.css';
-import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(<Pages />, document.getElementById('app'));
